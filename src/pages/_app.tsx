2c9f6ef3ec579db6c73ea47e90534747e872ccbd
Sarino MALALANIRINA <sarino.malalanirina@esti.mg>
2025-04-09 06:05:17 +0300

    feat : Refactor  de composant App pour inclure Navbar et Footer avec le style de mise en page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
-
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import { cn } from '@/lib/utils';
+import { Inter } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'] });
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={cn(inter.className, 'min-h-screen flex flex-col bg-gray-50')}>
+      <Navbar />
+      <main className="flex-1">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
+    </div>
+  );
 }
